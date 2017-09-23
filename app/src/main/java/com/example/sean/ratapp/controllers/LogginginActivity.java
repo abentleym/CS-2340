a7f169da1407afdebefc1a7bdfae27fb8955fb50
jfahey3 <jfahey3@gatech.edu>
2017-09-23 10:07:05 -0400

    added implementation to get to user home screen

--- app/src/main/java/com/example/sean/ratapp/controllers/LogginginActivity.java
+++ app/src/main/java/com/example/sean/ratapp/controllers/LogginginActivity.java
@@ -1,7 +1,9 @@
 package com.example.sean.ratapp.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +18,8 @@ public class LogginginActivity extends AppCompatActivity {
     private EditText userName;
     private EditText password;
     private Button logIn;
+    private Button error;
+    private int x = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,24 @@ public class LogginginActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.user_name);
         password = (EditText) findViewById(R.id.password);
         logIn = (Button) findViewById(R.id.enter_login);
+        error = (Button) findViewById(R.id.error);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (x == 10) {
+                    startActivity(new Intent(LogginginActivity.this, HomeActivity.class));
+                } else {
+                    error.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        error.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                error.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
