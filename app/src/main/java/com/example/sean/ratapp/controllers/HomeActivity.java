e5b990baa85857be9b7a56df4988cac5b4ffa915
jfahey3 <jfahey3@gatech.edu>
2017-09-23 09:26:08 -0400

    added logging in actvity and implementation for switching screens. Also added user class to model package/ added home activity screen

---
+++ app/src/main/java/com/example/sean/ratapp/controllers/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.sean.ratapp.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sean.ratapp.R;
+
+/**
+ * Created by jfahe on 9/23/2017.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
