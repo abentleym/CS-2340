7dbf9c1c4c80273a4487c26fc01f2b9603a30f91
jfahey3 <jfahey3@gatech.edu>
2017-10-13 13:04:23 -0400

    added add sighting screen

--- app/src/main/java/com/example/sean/ratapp/controllers/AddSightingActivity.java
+++ app/src/main/java/com/example/sean/ratapp/controllers/AddSightingActivity.java
@@ -2,6 +2,8 @@ package com.example.sean.ratapp.controllers;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sean.ratapp.R;
 
@@ -10,8 +12,31 @@ import com.example.sean.ratapp.R;
  */
 
 public class AddSightingActivity extends AppCompatActivity {
+    private EditText address;
+    private EditText city;
+    private EditText locationType;
+    private EditText zip;
+    private EditText borough;
+    private EditText date;
+    private EditText latitude;
+    private EditText longitude;
+    private Button addSighting;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sighting);
+
+        // setting up edit texts to corresponding variables
+        address = (EditText) findViewById(R.id.address);
+        city = (EditText) findViewById(R.id.city);
+        locationType = (EditText) findViewById(R.id.locationType);
+        zip = (EditText) findViewById(R.id.zipcode);
+        borough = (EditText) findViewById(R.id.borough);
+        date = (EditText) findViewById(R.id.date);
+        latitude = (EditText) findViewById(R.id.latitude);
+        longitude = (EditText) findViewById(R.id.longitude);
+
+        addSighting = (Button) findViewById(R.id.add);
+
     }
 }
--- app/src/main/java/com/example/sean/ratapp/model/RatSighting.java
+++ app/src/main/java/com/example/sean/ratapp/model/RatSighting.java
@@ -1,5 +1,8 @@
 package com.example.sean.ratapp.model;
 
+
+import java.util.Random;
+
 /**
  * Created by jfahe on 10/6/2017.
  */
@@ -30,6 +33,13 @@ public class RatSighting {
         this.borough = borough;
     }
 
+    //constructor used for adding rat sightings where uniqueId creates id
+    public RatSighting(String createdDate, String locationType, int zip, String address,
+                       String city, String borough, double latitude, double longitude) {
+        this(uniqueId(), createdDate, locationType,zip, address, city, borough, latitude,
+                longitude);
+    }
+
     public int getKey() { return key; }
     public String getDate() { return createdDate; }
     public String getLocation() { return locationType; }
@@ -40,6 +50,17 @@ public class RatSighting {
     public double getLatitude() { return latitude; }
     public double getLongitude() { return longitude; }
 
+    /**
+     * method used to generate random id number
+     * @return rand id number
+     */
+    public static int uniqueId() {
+        Random rand = new Random();
+        int id = rand.nextInt(100);
+        id = id * rand.nextInt(50);
+        id += rand.nextInt(100);
+        return id;
+    }
     // toString method to return some info as a string to be displayed in each entry in the ListView
     // that lists all rat sightings. Currently returns unique key of rat sighting.
     @Override
